15d78828898b61c0125b6e25595fc5edb988d238
Pushpender Singh <pushpender.singh@nagarro.com>
2026-01-13 15:41:17 +0530

    update functionality

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -12,11 +12,42 @@ namespace WebApp.Controllers
             return View(categories);
         }
 
+        // its a "HttpGet" Method ( automatically generated )
         public IActionResult Edit(int? id)
         {
             var category = CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
 
             return View(category);
         }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.UpdateCategory(category.CategoryId, category);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                CategoriesRepository.AddCategory(category);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
     }
-}
+} 
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Category
     {
+        [Required]
         public int CategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty; 
